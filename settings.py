dacef1f3ae9c9b57ca826992c5d8b129a846c7ba
Shubhangi Mali <malishubhangi552@gmail.com>
2025-12-19 18:55:16 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,3 @@
+SECRET_KEY='demo'
+INSTALLED_APPS=['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','student']
+DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':'db.sqlite3'}}
---
+++ views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Student
+def student_list(request):
+    students=Student.objects.all()
+    return render(request,'students.html',{'students':students})
